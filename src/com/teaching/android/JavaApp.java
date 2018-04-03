f8a78e1ee9f8f4c993711c7a4e7d1a30a0df969e
Manel Simon <manelizzard@gmail.com>
2018-04-03 11:25:19 +0200

    Initial Commit

---
+++ src/com/teaching/android/JavaApp.java
@@ -0,0 +1,12 @@
+package com.teaching.android;
+
+/**
+ * Empty Java App for teaching purposes.
+ */
+public class JavaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+}
