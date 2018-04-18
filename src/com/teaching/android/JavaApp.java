fa5f68afef05fb0af1c493a16c361ceecd3c5803
Jose Sierra <j_sierra_91@hotmail.com>
2018-04-18 10:07:37 +0200

    numeros par

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -1,12 +1,33 @@
 package com.teaching.android;
 
+import java.util.Scanner;
+
 /**
  * Empty Java App for teaching purposes.
  */
 public class JavaApp {
 
     public static void main(String[] args) {
-        System.out.println("peter griffin was here"); // Display the string.
-    }
+        System.out.println("escribe un numero");
+        Scanner scanner = new Scanner(System.in);
+        int valor = scanner.nextInt();
 
+            // forma 1 //
+
+            if (valor % 2 == 0) {
+                System.out.println("el numero es par");
+            } else {
+                System.out.println("el numero es impar");
+            }
+
+        // forma 2 //
+
+            int result = valor % 2;
+            if (result != 0) {
+                System.out.print("el numero es impar");
+            } else {
+                System.out.print("el numero es par");
+            }
+
+    }
 }
